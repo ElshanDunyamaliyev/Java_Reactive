28f40e0d6722b8015c3f411dc779159b064907fb
ElshanDunyamaliyev <elshandunyamaliyev@gmail.com>
2025-05-04 09:54:19 +0400

    change create with generate for assignment

--- src/main/java/sec04/assignment/FileReaderServiceImpl.java
+++ src/main/java/sec04/assignment/FileReaderServiceImpl.java
@@ -6,24 +6,56 @@ import java.io.*;
 import java.nio.file.Path;
 
 public class FileReaderServiceImpl implements FileReaderService{
+
     @Override
     public Flux<String> read(Path path) {
-        return Flux.create(fluxSink -> {
-            fluxSink.onRequest(request -> {
-                try {
-                    FileInputStream fs= new FileInputStream(path.toString());
-                    BufferedReader br = new BufferedReader(new InputStreamReader(fs));
-                    for (int i = 0; i < request; i++) {
-                        fluxSink.next(br.readLine());
-                    }
-                } catch (FileNotFoundException e) {
-                    fluxSink.error(e);
-                    throw new RuntimeException(e);
-                } catch (IOException e) {
-                    fluxSink.error(e);
-                    throw new RuntimeException(e);
+        return Flux.generate(() -> {
+            try {
+                var reader = new BufferedReader(new FileReader(path.toFile()));
+                return reader;
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }, (reader,sink) -> {
+            try {
+                String line = null;
+                line = reader.readLine();
+                if(line!= null){
+                    sink.next(line);
+                }else{
+                    sink.complete();
                 }
-            });
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return reader;
+        }, reader -> {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         });
     }
+
+//    @Override
+//    public Flux<String> read(Path path) {
+//        return Flux.create(fluxSink -> {
+//            fluxSink.onRequest(request -> {
+//                try {
+//                    FileInputStream fs= new FileInputStream(path.toString());
+//                    BufferedReader br = new BufferedReader(new InputStreamReader(fs));
+//                    for (int i = 0; i < request; i++) {
+//                        fluxSink.next(br.readLine());
+//                    }
+//                } catch (FileNotFoundException e) {
+//                    fluxSink.error(e);
+//                    throw new RuntimeException(e);
+//                } catch (IOException e) {
+//                    fluxSink.error(e);
+//                    throw new RuntimeException(e);
+//                }
+//            });
+//        });
+//    }
 }
